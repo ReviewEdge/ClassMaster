afb31333244f08e9c5ce36be2c14ed577cffda43
Logan Richardson <reviewedge56@gmail.com>
2023-02-22 15:22:05 -0500

    Initial commit

---
+++ src/main/java/edu/gcc/comp350/frg/Main.java
@@ -0,0 +1,8 @@
+package edu.gcc.comp350.frg;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello there");
+
+    }
+}
---
+++ src/test/java/edu/gcc/comp350/frg/MainTest.java
@@ -0,0 +1,12 @@
+package edu.gcc.comp350.frg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+}
