991deea32590792044cd33bda8a88efd57913118
Logan Richardson <reviewedge56@gmail.com>
2023-02-27 16:02:38 -0500

    setup Search

--- src/main/java/edu/gcc/comp350/frg/Search.java
+++ src/main/java/edu/gcc/comp350/frg/Search.java
@@ -1,4 +1,35 @@
 package edu.gcc.comp350.frg;
 
+import java.util.ArrayList;
+
 public class Search {
+
+    private String query;
+    private Filter filter;
+    private ArrayList<Class> currentResults;
+
+    public Search(String query, Filter filter) {
+        this.query = query;
+        this.filter = filter;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public Filter getFilter() {
+        return filter;
+    }
+
+    public void setFilter(Filter filter) {
+        this.filter = filter;
+    }
+
+    public ArrayList<Class> getCurrentResults() {
+        return currentResults;
+    }
 }
