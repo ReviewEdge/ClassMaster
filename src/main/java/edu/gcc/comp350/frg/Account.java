94ed2fb5a4b9b631580ed1edd7f9e91889ada79b
TZIOVANNISCM21 <TziovannicCM21@gcc.edu>
2023-02-27 15:54:06 -0500

    made a class class

--- src/main/java/edu/gcc/comp350/frg/Account.java
+++ src/main/java/edu/gcc/comp350/frg/Account.java
@@ -1,4 +1,29 @@
 package edu.gcc.comp350.frg;
 
+import java.util.ArrayList;
+
 public class Account {
+
+    private int id;
+    private String name;
+    private String eMail;
+    private String password;
+    private String username;
+    private ArrayList<Schedule> mySchedules;
+
+    public Account(int id, String name, String eMail, String password, String username) {
+        this.id = id;
+        this.name = name;
+        this.eMail = eMail;
+        this.password = password;
+        this.username = username;
+        this.mySchedules = new ArrayList<Schedule>();
+    }
+
+    public boolean login(String checkPassword){
+        return false;
+    }
+    public boolean logout(){
+        return false;
+    }
 }
--- src/main/java/edu/gcc/comp350/frg/Term.java
+++ src/main/java/edu/gcc/comp350/frg/Term.java
@@ -1,4 +1,20 @@
 package edu.gcc.comp350.frg;
 
 public class Term {
+
+    private int id;
+    private String name;
+
+    public Term(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
