202211de45258e8454b6c480c1cc76cbffd5c8a6
TZIOVANNISCM21 <TziovannicCM21@gcc.edu>
2023-02-27 15:32:34 -0500

    Made the methods for the Schedule class (help)

--- src/main/java/edu/gcc/comp350/frg/Class.java
+++ src/main/java/edu/gcc/comp350/frg/Class.java
@@ -1,4 +1,7 @@
 package edu.gcc.comp350.frg;
 
 public class Class {
+
+    public Class(Class c) {
+    }
 }
--- src/main/java/edu/gcc/comp350/frg/Schedule.java
+++ src/main/java/edu/gcc/comp350/frg/Schedule.java
@@ -1,6 +1,66 @@
 package edu.gcc.comp350.frg;
 
+import java.util.ArrayList;
+
 public class Schedule {
+
+    private String name;
+    private int id;
+    private Term term;
+    private ArrayList<Class> classes;
+
+    public Schedule(String name, int id, Term term) {
+        this.name = name;
+        this.id = id;
+        this.term = term;
+        this.classes = new ArrayList<>();
+    }
+    //dont worry about this yet pls
+//    public Schedule( Schedule other) {
+//        this.name = other.name;
+//        this.id = other.id;
+//        this.term = other.term;
+//        this.classes = new ArrayList<>();
+//        for(Class c: other.classes){
+//            this.classes.add(new Class(c));
+//        }
+//    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Term getTerm() {
+        return term;
+    }
+
+    public ArrayList<Class> getClasses() {
+        return classes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addClass (Class toAdd) throws Exception{
+
+    }
+    public void removeClass (Class toRemove) throws Exception{
+
+    }
+
+    public ArrayList<Integer> getRefNums(){
+        return null;
+    }
+
+    public ArrayList<String> getClassNames(){
+        return null;
+    }
 
 }
 
