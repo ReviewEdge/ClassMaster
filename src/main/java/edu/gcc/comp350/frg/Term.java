d0b04d8a06b7ce43e47ce3b7d2ed871ecf615a03
Logan Richardson <reviewedge56@gmail.com>
2023-03-31 16:01:01 -0400

    make terms a real thing

--- src/main/java/edu/gcc/comp350/frg/Term.java
+++ src/main/java/edu/gcc/comp350/frg/Term.java
@@ -5,6 +5,30 @@ public class Term {
     private int id;
     private String name;
 
+    // TODO: I'm not sure how to handle invalid term input. Shouldn't be a problem right now though,
+    //  because all of the classes in the DB have a valid trm_cde
+    public Term(int id) {
+        this.id = id;
+        if (id == 10) {
+            this.name = "Fall 2020";
+        } else if (id == 30) {
+            this.name = "Spring 2021";
+        } else {
+            this.name = null;
+        }
+    }
+
+    public Term(String name) {
+        this.name = name;
+        if (name.equals("Fall 2020")) {
+            this.id = 10;
+        } else if (name.equals("Spring 2020")) {
+            this.id = 30;
+        } else {
+            this.id = 0;
+        }
+    }
+
     public Term(int id, String name) {
         this.id = id;
         this.name = name;
@@ -22,6 +46,8 @@ public class Term {
     public String toString() {
         return name;
     }
+
+    // TODO: not sure if other == null should default to false
     @Override
     public boolean equals(Object other){
         if(other == null) return false;
