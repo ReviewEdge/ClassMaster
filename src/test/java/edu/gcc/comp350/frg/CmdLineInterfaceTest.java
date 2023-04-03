8bde9c3a6c8d2d135a3b21085b855a7ddb1b2403
Jaxaar <jax1664-cs@yahoo.com>
2023-04-02 22:53:35 -0400

    Bunch o Last minute changes
    Moved db file to resources
    Added /n to calendar printing
    Added a way to remove schedules. Not a good way

--- src/test/java/edu/gcc/comp350/frg/CmdLineInterfaceTest.java
+++ src/test/java/edu/gcc/comp350/frg/CmdLineInterfaceTest.java
@@ -47,4 +47,10 @@ class CmdLineInterfaceTest {
         API api = new API(main);
         CmdLineInterface.runInterface(api, true, "TestFiles/classConflictTest");
     }
+
+    //Only used for cleaning up the database... NOT ACTUALLY A TEST, Just needed a way to run the cmd
+//    @Test
+//    void deleteScheduleCmd(){
+//        Schedule.deleteScheduleByIDFromDB(1);
+//    }
 }
