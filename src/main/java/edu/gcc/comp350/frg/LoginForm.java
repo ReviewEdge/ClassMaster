e4459382c6db52a1a27a1db5fc30d69fa0a8f47c
Logan Richardson <reviewedge56@gmail.com>
2023-04-30 19:39:44 -0400

    add in missing LoginForm

---
+++ src/main/java/edu/gcc/comp350/frg/LoginForm.java
@@ -0,0 +1,19 @@
+package edu.gcc.comp350.frg;
+
+public class LoginForm {
+    private String email;
+    private String password;
+
+    public LoginForm(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
