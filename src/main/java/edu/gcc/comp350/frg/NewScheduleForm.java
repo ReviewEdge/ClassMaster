bcc1f5082d926d1336799f9c3225cfcd4fc83dd9
Logan Richardson <reviewedge56@gmail.com>
2023-05-01 23:19:05 -0400

    All backend for getting and creating schedules

--- src/main/java/edu/gcc/comp350/frg/NewScheduleForm.java
+++ src/main/java/edu/gcc/comp350/frg/NewScheduleForm.java
@@ -1,2 +1,25 @@
-package edu.gcc.comp350.frg;public class NewScheduleForm {
+package edu.gcc.comp350.frg;
+
+public class NewScheduleForm {
+    private String loginSecret;
+    private String name;
+    private String term;
+
+    public NewScheduleForm(String loginSecret, String name, String term) {
+        this.loginSecret = loginSecret;
+        this.name = name;
+        this.term = term;
+    }
+
+    public String getLoginSecret() {
+        return loginSecret;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTerm() {
+        return term;
+    }
 }
