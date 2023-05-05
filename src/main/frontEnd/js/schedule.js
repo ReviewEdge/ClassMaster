4c87db83742d7b5bd9e72b4b76975eb00a137fe5
Jaxaar <jax1664-cs@yahoo.com>
2023-05-05 11:15:32 -0400

    Added Chris's Code "PseudoMerge"

--- src/main/frontEnd/js/schedule.js
+++ src/main/frontEnd/js/schedule.js
@@ -15,10 +15,53 @@ import { coFactory } from './courseInfo.js';
 //         });
 // });
 
+// create an array of Class objects
+const classes = [
+    new Class('Math 101', '8:00 AM', '8:30 AM', 'Sunday'),
+    new Class('Physics 201', '9:00 AM', '10:30 AM', 'Tuesday'),
+    new Class('English 202', '11:00 AM', '12:30 PM', 'Monday'),
+    // more Class objects
+    ];
+
+    // loop through each Class object and add it to the table cell that corresponds to its day and time
+    const table = document.getElementById('calendar-view-table');
+    classes.forEach(cls => {
+    const cell = table.querySelector('tr:nth-child(${getRowIndex(cls.startTime)}) td:nth-child(${getColumnIndex(cls.dayOfWeek)})');
+    cell.innerText = "hello?";
+});  
+
+
 
 window.addEventListener("DOMContentLoaded",function() {
     updateSchedule()
 });
+
+/* AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                    This is new stuff           **/
+
+// define your Java objects (for example, a Class object)
+class Class {
+    constructor(name, startTime, endTime, dayOfWeek) {
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.dayOfWeek = dayOfWeek;
+    }
+}
+
+  
+  
+// helper function to get the column index of a day of the week
+function getColumnIndex(dayOfWeek) {
+const columns = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+return columns.indexOf(dayOfWeek) + 1;
+}
+function getRowIndex(classTime) {
+const rows = ['8:00 AM','8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM','1:00 PM','1:30 PM','2:00 PM','2:30 PM','3:00 PM','3:30 PM','4:00 PM','4:30 PM','5:00 PM','5:30 PM','6:00 PM','6:30 PM','7:00 PM','7:30 PM',];
+return columns.indexOf(classTime) + 1;
+}
+  
+  
 
 window.addEventListener("DOMContentLoaded",function() {
     getMyScheduleNames();
