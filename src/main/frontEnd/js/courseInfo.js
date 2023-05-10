3e7e2d8a64bd8444631d73e20412b087b57aaac9
Jaxaar <jax1664-cs@yahoo.com>
2023-05-10 17:08:45 -0400

    Removed depreciated method, Yay

--- src/main/frontEnd/js/courseInfo.js
+++ src/main/frontEnd/js/courseInfo.js
@@ -22,61 +22,6 @@ class classObjectFactory {
         coFactoryExists = true;
     }
 
-    createClassObject(classInfo, inSchedule) {
-        // console.log(classInfo)
-        const p = document.createElement("p");
-        p.className = 'classText';
-        p.id ='classText' + this.nextUniqueID;
-    //    p.innerText = ClassInfo + "     (•••)";
-        p.innerText = classInfo;
-        // p.setAttribute('onclick', 'clickMoreInfo(' + this.nextUniqueID + ')');
-
-        const detailsButton = document.createElement("span");
-        // detailsButton.className = 'classText';
-        detailsButton.id ='detailsButton' + this.nextUniqueID;
-        detailsButton.innerHTML = "<br><b>Click for Details</b> ";
-        // detailsButton.innerText = "(Click for Details) ";
-        const intVal = this.nextUniqueID
-        detailsButton.addEventListener("click", function() {
-            var popup = document.getElementById('classInfoText' + intVal);
-            popup.classList.toggle("show");
-        });
-        p.append(detailsButton)
-
-        var classInfoSplit = classInfo.split(",");
-        // console.log(classInfoSplit)
-
-        if(!(inSchedule)){
-            const addButton = document.createElement("span");
-            addButton.className = 'btn btn-success btn-sm addClassButton';
-            addButton.id ='addClassButton' + this.nextUniqueID;
-            addButton.innerText = "+";
-            addButton.addEventListener("click", function() {
-                addClassToSchedule(classInfoSplit[0])
-            });
-            p.append(addButton)
-        }
-        else {
-            const removeButton = document.createElement("span");
-            removeButton.className = 'btn btn-danger btn-sm removeClassButton';
-            removeButton.id ='removeClassButton' + this.nextUniqueID;
-            removeButton.innerText = "-";
-            removeButton.addEventListener("click", function() {
-                removeClassFromSchedule(classInfoSplit[0])
-            });
-            p.append(removeButton)
-        }
-
-        const pop = document.createElement("p");
-        pop.className = 'classInfoText';
-        pop.id ='classInfoText' + this.nextUniqueID;
-        pop.innerText = "Get Description Information: WIP";
-        p.append(pop);
-
-        this.nextUniqueID++;
-        return p;
-    }
-
     createClassObjectFromJSON(classInfoJSON, inSchedule) {
         // console.log(classInfoJSON)
         const p = document.createElement("p");
